21db0f3c6e8c257b992fd6e45f98491583fc1565
Dmytro Mariukhna <maryukhna05@gmail.com>
2025-11-01 22:08:40 +0200

    test routing and data retrieval from the database

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/users', function () {
+    $users = User::all();
+    return $users;
 });
