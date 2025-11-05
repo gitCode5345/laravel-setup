da2f1a16d3ed334ebc67e3ac7b743c13aec77d3b
Dmytro Mariukhna <maryukhna05@gmail.com>
2025-11-05 19:51:29 +0200

    Endpoint implementation removed

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,7 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/users', function () {
-    $users = User::all();
-    return $users;
 });
